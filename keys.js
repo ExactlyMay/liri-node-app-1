ce011f1063440cfaaefe319d46f293916dde93b7
jordonbowen <jordon_bowen@yahoo.com>
2018-07-24 21:30:19 -0400

    did it

---
+++ keys.js
@@ -0,0 +1,13 @@
+var twitterKeys = {
+	consumer_key: 'xNfBYKS1P85fDjfid5u3pUiW4',
+	consumer_secret: 'KL9oCgBpulvhQdzlwsArBNaSGUQnXigqOsIZF3czkuMeGgFc7U',
+	access_token_key: '1021805380593172480-oUh2cF6bkb7dA0V5gL3gextlgBP73d',
+	access_token_secret: 'cMIov8wON2ck8vWsZmDx8JF8aClvVjsxBjnOSNW4fdNoy',
+};
+
+var spotifyKeys = {
+	id: '66a99d99226c47aca99dfbb4a325545d',
+	secret: 'a4c5ebc8e6004ea1947f880102a7c520',
+};
+
+module.exports = { twitterKeys, spotifyKeys };
